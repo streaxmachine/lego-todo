d1fe6165657a24d778c7ca8cefd6408623cb163f
streax <lesha.streax@mail.ru>
2022-04-04 16:32:44 +0300

    5

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import FormEvents from './Experience/FormEvents'
 
 const experience = new Experience(document.querySelector("canvas.webgl"))
 const formevents = new FormEvents()
+CI = false
 // import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // import * as dat from 'lil-gui'
