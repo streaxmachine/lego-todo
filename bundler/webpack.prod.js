18fa5c12926f66594760d7134678e3ed776010d4
streax <lesha.streax@mail.ru>
2022-04-04 17:46:57 +0300

    20

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,13 +1,14 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 module.exports = merge(
     commonConfiguration,
     {
         mode: 'production',
         output: {
-            publicPath: 'auto',
+            publicPath: ASSET_PATH,
           },
         plugins:
         [
